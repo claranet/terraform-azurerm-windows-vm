ff8bfe4684da6d5d4098f0f615d963ffa5719537
Benjamin DUPUIS <benjamin.dupuis@fr.clara.net>
2019-12-02 13:34:21 +0100

    AZ-153: Fix XML Password containing special chars

--- locals.tf
+++ locals.tf
@@ -12,4 +12,6 @@ locals {
   custom_data_params = "Param($ComputerName = \"${local.vm_name}\")"
 
   custom_data_content = "${local.custom_data_params} ${file(format("%s/files/winrm.ps1", path.module))}"
+
+  admin_password_encoded = replace(replace(replace(replace(replace(var.admin_password, "&[^#]", "&#38;"), ">", "&#62;"), "<", "&#60;"), "'", "&#39;"), "\"", "&#34;")
 }
