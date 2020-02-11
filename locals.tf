c894d20b5cecb578916e4651d358de84f113a611
Laurent Piroelle <laurent.piroelle@fr.clara.net>
2020-02-11 11:01:38 +0100

    AZ-180: Guess OS values

--- locals.tf
+++ locals.tf
@@ -3,8 +3,8 @@ locals {
     env              = var.environment
     stack            = var.stack
     os_family        = "windows"
-    os_distribution  = "undefined"
-    os_version       = "undefined"
+    os_distribution  = lookup(var.vm_image, "offer", "undefined")
+    os_version       = lookup(var.vm_image, "sku", "undefined")
     patch_management = "enabled"
   }
 
