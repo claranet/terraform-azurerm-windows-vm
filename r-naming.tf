3a25028322aa98a886f41ef528f64cdc6d4c0d37
adrien.rodenburg <adrien.rodenburg@fr.clara.net>
2023-03-02 15:54:52 +0100

    AZ-1018 Added suffix to storage naming

--- r-naming.tf
+++ r-naming.tf
@@ -44,7 +44,7 @@ data "azurecaf_name" "disk" {
   name          = var.stack
   resource_type = "azurerm_managed_disk"
   prefixes      = var.name_prefix == "" ? null : [local.name_prefix]
-  suffixes      = compact([var.client_name, var.location_short, var.environment, each.key])
+  suffixes      = compact([var.client_name, var.location_short, var.environment, local.name_suffix, each.key])
   use_slug      = var.use_caf_naming
   clean_input   = true
   separator     = "-"
