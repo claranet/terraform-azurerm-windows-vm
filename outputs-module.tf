80c82c1340645923eaa0587f9beeb984b9017fda
Jérôme Respaut <jerome.respaut@claranet.com>
2024-04-26 15:40:12 +0200

    style(output): remove unused version from outputs-module

--- outputs-module.tf
+++ outputs-module.tf
@@ -2,7 +2,6 @@ output "terraform_module" {
   description = "Information about this Terraform module"
   value = {
     name       = "windows-vm"
-    version    = file("${path.module}/VERSION")
     provider   = "azurerm"
     maintainer = "claranet"
   }
